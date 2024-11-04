4056116b1f8d84827c905fe024ccf8f717162fb5
Kevin R <kruan8128@gmail.com>
2024-11-04 16:18:25 -0500

    working for create, add, delete expense

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const readline = require('readline');
 
+/**
+ * {
+ * name
+ * cost
+ * }
+ */
 const expenses = [];
 
 const rl = readline.createInterface({
@@ -11,6 +17,12 @@ function prompt(questionText, callback) {
     rl.question(questionText, (input) => {
         if (input === '--help') {
             displayHelp();
+        } else if (input.includes(':add')) {
+            const [_, formattedAddStr] = input.split(' ');
+            const [name, cost] = formattedAddStr.split(',');
+            expenses.push({ name, cost });
+            console.log('\nSuccessfully added the expense!');
+            displayMain();
         } else {
             callback(input);
         }
@@ -19,7 +31,7 @@ function prompt(questionText, callback) {
 
 function displayHome() {
     console.clear();
-    console.log('Welcome to the MyFinance CLI App!\n');
+    console.log('\nWelcome to the MyFinance CLI App!\n');
     console.log(
         `This is a finance tracking application designed with developers in mind.\nIf you’re already familiar with using cli commands, this app will be very familiar to you!`
     );
@@ -43,20 +55,27 @@ function displayMain() {
     console.log(`3. Delete Expense`);
 
     prompt('Input: ', (input) => {
-        if (input === '2') {
+        if (input === '1') {
+            displayViewExpenses();
+        } else if (input === '2') {
             displayAddExpense();
+        } else if (input === '3') {
+            displayDeleteExpense();
+        } else {
+            console.log('You entered an invalid input.');
+            displayMain();
         }
     });
 }
 
 function displayHelp() {
     console.clear();
-    console.log('--- Help ---\n');
+    console.log('\n--- Help ---\n');
     console.log('Input 1 to return to main menu\n');
     console.log(
         'These are shortcut commands that can be typed at any time to help you.\n'
     );
-    console.log(':add name,amount,type to quickly add an expense.');
+    console.log(':add name,amount to quickly add an expense.');
     console.log(':quit to exit the application.\n');
 
     prompt('Input: ', (input) => {
@@ -66,6 +85,22 @@ function displayHelp() {
     });
 }
 
+function displayViewExpenses() {
+    console.log(`\n--- View Expenses---`);
+    if (expenses.length === 0) {
+        console.log('You have no expenses.');
+    } else {
+        console.log('Your Expenses: ');
+        for (let i = 0; i < expenses.length; i++) {
+            const expense = expenses[i];
+            console.log(
+                `Id: ${i}, Name: ${expense.name}, Cost: $${expense.cost}`
+            );
+        }
+    }
+    displayMain();
+}
+
 function displayAddExpense() {
     console.log(`\n--- Add Expense ---`);
     const expense = {};
@@ -73,14 +108,37 @@ function displayAddExpense() {
         expense.name = input;
         prompt('Enter expense cost: ', (input) => {
             expense.cost = input;
-            prompt('Enter expense type: ', (input) => {
-                expense.type = input;
-                expenses.push(expense);
-                console.log('Expense successfully added!');
-                displayMain();
-            });
+            expenses.push(expense);
+            console.log('\nExpense successfully added!');
+            displayMain();
         });
     });
+}
+
+function displayDeleteExpense() {
+    console.log(`\n--- Delete Expense ---`);
+    if (expenses.length === 0) {
+        console.log('You have no expenses to delete.');
+    } else {
+        prompt('Enter the Id of the expense you want to delete: ', (input) => {
+            let indexToDelete = input;
+            prompt(
+                `Please confirm you want to delete ${expenses[input].name} expense. (y/n): `,
+                (input) => {
+                    if (input === 'y' || input === 'yes') {
+                        expenses.splice(indexToDelete, 1);
+                        console.log('\nExpense deleted successfully!');
+                        displayMain();
+                    } else {
+                        console.log(
+                            'You did not confirm to delete the expense.'
+                        );
+                        displayMain();
+                    }
+                }
+            );
+        });
+    }
 }
 
 displayHome();
