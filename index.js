b2e91f86d3de079e045875a80148326c25ded2b1
Kevin R <kruan8128@gmail.com>
2024-12-02 20:25:47 -0500

    implemented req for microservices

--- index.js
+++ index.js
@@ -48,16 +48,12 @@ function displayHome() {
     console.log(
         `This is a finance tracking application designed with developers in mind.\nIf you’re already familiar with using cli commands, this app will be very familiar to you!`
     );
-    console.log(
-        'Note: Data is stored temporarily and will be lost when the app is closed.'
-    );
-    console.log('Type "--help" at any time to view more information.\n');
-    console.log('Input 1 to view Main Menu.\n');
 
-    prompt('Input: ', (input) => {
-        if (input === '1') {
-            displayMain();
-        }
+    console.log('Type "--help" at any time to view more information.\n');
+    console.log('Input anything to view Main Menu.\n');
+
+    prompt('Input: ', (_) => {
+        displayMain();
     });
 }
 
@@ -66,14 +62,142 @@ function displayMain() {
     console.log(`1. View Expense`);
     console.log(`2. Add Expense`);
     console.log(`3. Delete Expense`);
+    console.log(`4. Display total spendings`);
+    console.log(`5. Display spending by category`);
+    console.log(`6. Get a random quote to feel inspired`);
+    console.log(`7. Add a quote to your pool of quotes`);
+    console.log(`8. Get all currency rates`);
+    console.log(`9. Convert a currency to another currency`);
+    console.log(`10. Add a currency exchange rate`);
 
-    prompt('Input: ', (input) => {
+    prompt('Input: ', async (input) => {
         if (input === '1') {
             displayViewExpenses();
         } else if (input === '2') {
             displayAddExpense();
         } else if (input === '3') {
             displayDeleteExpense();
+        } else if (input === '4') {
+            try {
+                const { data } = await axios.get(
+                    'http://localhost:3002/expenses/total'
+                );
+                console.log(`\nTotal spendings: ${data.totalCost}`);
+                displayMain();
+            } catch (error) {
+                console.error(error);
+            }
+        } else if (input === '5') {
+            try {
+                const { data: categoricalExpenses } = await axios.get(
+                    'http://localhost:3002/expenses/category'
+                );
+                console.table(
+                    categoricalExpenses.map((expense) => ({
+                        Category: expense.category,
+                        'Total Cost': expense.total_cost,
+                    }))
+                );
+            } catch (error) {
+                console.error(error);
+            }
+            displayMain();
+        } else if (input === '6') {
+            try {
+                const { data } = await axios.get(
+                    'http://localhost:3003/quotes/random'
+                );
+                console.log(`\n${data.text}`);
+            } catch (error) {
+                console.error(error);
+            }
+            displayMain();
+        } else if (input === '7') {
+            prompt('What is the quote you want to add? ', async (input) => {
+                const reqBody = { text: input };
+                try {
+                    await axios.post('http://localhost:3003/quotes', reqBody);
+                    console.log(`\nSuccessfully added quote!`);
+                    displayMain();
+                } catch (error) {
+                    console.error(error);
+                }
+            });
+        } else if (input === '8') {
+            try {
+                const { data: exchangeRates } = await axios.get(
+                    'http://127.0.0.1:5000/list'
+                );
+                console.table(
+                    exchangeRates.map((rate) => ({
+                        'From Currency': rate.from_currency,
+                        'To Currency': rate.to_currency,
+                        Rate: rate.rate,
+                    }))
+                );
+                displayMain();
+            } catch (error) {
+                console.error(error);
+            }
+        } else if (input === '9') {
+            const reqParams = {};
+            prompt(
+                'What is the from currency you want to convert? ',
+                (input) => {
+                    reqParams.from_currency = input;
+                    prompt(
+                        `What is the amount of currency you want to convert? `,
+                        (input) => {
+                            reqParams.amount = input;
+                            prompt(
+                                `What currency you want to convert to? `,
+                                async (input) => {
+                                    reqParams.to_currency = input;
+                                    try {
+                                        const { data } = await axios.get(
+                                            'http://127.0.0.1:5000/convert',
+                                            { params: reqParams }
+                                        );
+                                        console.log(
+                                            `\nThe conversion is ${
+                                                data.to_currency
+                                            } ${data.converted_amount.toFixed(
+                                                2
+                                            )}`
+                                        );
+                                        displayMain();
+                                    } catch (error) {
+                                        console.error(error);
+                                    }
+                                }
+                            );
+                        }
+                    );
+                }
+            );
+        } else if (input === '10') {
+            const reqBody = {};
+            prompt('What is the from currency you want to add? ', (input) => {
+                reqBody.from_currency = input;
+                prompt(`What is the to currency you want to add? `, (input) => {
+                    reqBody.to_currency = input;
+                    prompt(`What is the exchange rate? `, async (input) => {
+                        reqBody.rate = input;
+                        try {
+                            await axios.post(
+                                'http://127.0.0.1:5000/add',
+                                reqBody
+                            );
+                            console.log(
+                                `\nSuccessfully added new currency exchange`
+                            );
+                            displayMain();
+                        } catch (error) {
+                            console.error(error);
+                        }
+                    });
+                });
+            });
         } else {
             console.log('You entered an invalid input.');
             displayMain();
